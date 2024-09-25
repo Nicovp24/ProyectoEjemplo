c4c0b103d90d40f491b4e3cea272a25e43433bd7
Nicolás Vicenti <117927554+Nicovp24@users.noreply.github.com>
2024-09-25 12:24:13 +0200

    Añadir metodo saludar

---
+++ src/Saludo.java
@@ -0,0 +1,10 @@
+public class Saludo {
+    public void saludar() {
+        System.out.println("¡Hola, mundo!");
+    }
+
+    public static void main(String[] args) {
+        Saludo saludo = new Saludo();
+        saludo.saludar();
+    }
+}
